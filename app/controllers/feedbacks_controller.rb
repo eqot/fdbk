8c04bb8d5b5ea5d110f0a6737f1859655424b7e3
Ikuo Terado <eqobar@gmail.com>
2014-12-14 01:21:06 +0900

    Support creating feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -6,4 +6,25 @@ class FeedbacksController < ApplicationController
   def show
     @feedback = Feedback.find_by(params[:id])
   end
+
+  def new
+    @feedback = current_user.feedbacks.build
+  end
+
+  def create
+    @feedback = current_user.feedbacks.build(feedback_params)
+    if @feedback.save
+      redirect_to @feedback, notice: 'Created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(
+      :title, :description
+    )
+  end
 end
