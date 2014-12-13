4f490b766e8621c534a0e7cfa4af492b2f6f237a
Ikuo Terado <eqobar@gmail.com>
2014-12-14 02:08:43 +0900

    Allow iframe

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module FDBK
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # Allow iframe in other sites
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOWALL'
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
