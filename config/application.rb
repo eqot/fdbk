b2b797f0613231a3d80087fb9e25c02f7815b257
Ikuo Terado <eqobar@gmail.com>
2014-12-18 02:04:41 +0900

    Support CORS

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,12 @@ module FDBK
 
     # Add utility modules
     config.autoload_paths += Dir[Rails.root.join('lib', 'autoload')]
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
